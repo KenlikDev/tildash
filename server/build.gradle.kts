7d396b54e9461b3f919952ff6f1e4c10ba72f5c8
KenlikDev <kenlik.dev@proton.me>
2026-09-21 12:35:07 +0300

    build(server): apply Kotlin Spring plugin

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.kotlinSpring)
     alias(libs.plugins.springBoot)
     alias(libs.plugins.springDependencyManagement)
 }
