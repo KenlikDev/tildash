fcb547f6ddf468f8ff8b28aa388674db7d5be836
KenlikDev <kenlik.dev@proton.me>
2026-09-21 17:42:30 +0300

    chore(code-quality): add ktlint and jacoco foundations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+
 plugins {
     // Load shared plugins once in the root project classloader.
     alias(libs.plugins.androidApplication) apply false
@@ -7,6 +9,15 @@ plugins {
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinSpring) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.springBoot) apply false
     alias(libs.plugins.springDependencyManagement) apply false
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<KtlintExtension> {
+        version.set("1.8.0")
+    }
 }
